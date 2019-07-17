b1e1e10b1c6730f1ef22ad26283dc91dc431bc65
MichaelDc86 <lenskymiwa@ya.ru>
2019-07-17 16:35:11 +0300

    1

--- vist/vist/settings.py
+++ vist/vist/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-storage.ManifestStaticFilesStorage.manifest_strict = False
+# storage.ManifestStaticFilesStorage.manifest_strict = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
