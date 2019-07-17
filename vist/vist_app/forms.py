b2826bd5caa9fd3c46dd8cc4e18378e76023ddd2
MichaelDc86 <lenskymiwa@ya.ru>
2019-07-17 12:24:03 +0300

    1

--- vist/vist_app/forms.py
+++ vist/vist_app/forms.py
@@ -1,27 +1,23 @@
 from django import forms
-# from vist_app.models import TracksChoice
-#
-# chs = TracksChoice.objects.all()
+from vist_app.models import TracksChoice
 
-track_models_tuple = (
-    ('ВСЕ', 'ВСЕ',),
-    ('Белаз', 'Белаз',),
-    ('Komatsu', 'Komatsu',),
-)
+chs = TracksChoice.objects.all()
 
 # track_models_tuple = (
-#     (chs[0].trk_model, chs[0].trk_model,),
-#     (chs[1].trk_model, chs[1].trk_model,),
-#     (chs[2].trk_model, chs[2].trk_model,),
+#     ('ВСЕ', 'ВСЕ',),
+#     ('Белаз', 'Белаз',),
+#     ('Komatsu', 'Komatsu',),
 # )
 
-
-# class Tracksmodelform(forms.Form):
-#
-#     model = TracksChoice
-#     trk_model = forms.ChoiceField(choices=track_models_tuple)
+track_models_tuple = (
+    (chs[0].trk_model, chs[0].trk_model,),
+    (chs[1].trk_model, chs[1].trk_model,),
+    (chs[2].trk_model, chs[2].trk_model,),
+)
 
 
 class Tracksmodelform(forms.Form):
 
+    model = TracksChoice
     trk_model = forms.ChoiceField(choices=track_models_tuple)
+
