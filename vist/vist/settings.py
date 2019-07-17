7de045448c5258420f8efbd82c17ed2bd90c0f86
MichaelDc86 <lenskymiwa@ya.ru>
2019-07-17 16:38:25 +0300

    1

--- vist/vist/settings.py
+++ vist/vist/settings.py
@@ -170,6 +170,7 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
