e0e54ddeb66fb4c8c5507b031aa7f90be0da1aab
MichaelDc86 <41547313+MichaelDc86@users.noreply.github.com>
2019-07-17 09:57:05 +0300

    Update settings.py

--- vist/vist/settings.py
+++ vist/vist/settings.py
@@ -31,6 +31,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+# django_heroku.settings(locals())
 
 # Application definition
 
@@ -46,14 +47,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'vist.urls'
@@ -136,5 +137,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# django_heroku.settings(locals())
